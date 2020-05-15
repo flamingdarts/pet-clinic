e0407d89b8350d8e954673b159483caf81e9b270
Danh Nhan Phung <danhnhan.phung@gmail.com>
2020-05-15 10:44:38 +0200

    initial commit

---
+++ src/main/java/com/dnpi/petclinic/dnpipetclinic/DnpiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.dnpi.petclinic.dnpipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnpiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnpiPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dnpi/petclinic/dnpipetclinic/DnpiPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnpi.petclinic.dnpipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnpiPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

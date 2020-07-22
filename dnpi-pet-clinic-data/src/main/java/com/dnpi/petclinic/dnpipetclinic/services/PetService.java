4757a16567ce1ccce76d6aab04153fdebc8e0a1f
Danh Nhan Phung <danhnhan.phung@gmail.com>
2020-07-22 21:35:32 +0200

    refactor service. closes #12

--- dnpi-pet-clinic-data/src/main/java/com/dnpi/petclinic/dnpipetclinic/services/PetService.java
+++ dnpi-pet-clinic-data/src/main/java/com/dnpi/petclinic/dnpipetclinic/services/PetService.java
@@ -3,10 +3,5 @@ package com.dnpi.petclinic.dnpipetclinic.services;
 
 import com.dnpi.petclinic.dnpipetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- dnpi-pet-clinic-data/src/main/java/com/dnpi/petclinic/dnpipetclinic/services/VetService.java
+++ dnpi-pet-clinic-data/src/main/java/com/dnpi/petclinic/dnpipetclinic/services/VetService.java
@@ -2,10 +2,5 @@ package com.dnpi.petclinic.dnpipetclinic.services;
 
 import com.dnpi.petclinic.dnpipetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }

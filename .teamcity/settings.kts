45f13a0b1bd51b18d03dbbd9c00ae947d16dabab
Danh Nhan Phung <danhnhan.phung@gmail.com>
2020-07-22 13:33:53 +0200

    Implement services. Closes #5

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -38,7 +38,8 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
-        checkoutMode = CheckoutMode.ON_AGENT
+        checkoutMode = CheckoutMode.MANUAL
+
     }
 
     steps {
